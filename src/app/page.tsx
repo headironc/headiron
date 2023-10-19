b0785878189a033b3ff3864d2f4b51daf681f653
headironc <i@liangzhicheng.cc>
2023-10-19 23:24:17 +0800

    feat: config shadcn ui and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Click Me</Button>
+    </main>
+  )
 }
